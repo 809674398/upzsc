afccd798098d2cfb44de6e7f17b9b43578be05eb
zhousicong <zhousicong@zijinph.com>
2020-05-11 16:15:30 +0800

    Initial commit

---
+++ src/main/java/com/up/Application.java
@@ -0,0 +1,17 @@
+package com.up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: Application
+ * @Description: 启动类
+ * @author: zsc
+ * @date: 2020/5/11 9:22
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/com/up/rest/TestRestController.java
@@ -0,0 +1,23 @@
+package com.up.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: TestRestController
+ * @Description: 测试controller
+ * @author: zsc
+ * @date: 2020/5/11 9:17
+ */
+@RestController
+@RequestMapping("/up")
+public class TestRestController {
+
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+
+}
